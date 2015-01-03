076fe5f3b8c99648adbabffc6d6341b9e5f9edb3
Mr1024 <publiczhongemail@163.com>
2015-01-03 17:32:15 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-(function(){
-  
-})();
+(function(window) {
+    var macvideo = {
+        canPlayM3U8: (!!document.createElement("video").canPlayType("application/x-mpegURL")),
+        convertPatternToReg: function(pattern) {
+            var regexCharacters = '\\[](){}^$-.*+?|,/';
+            if (typeof pattern === 'string') {
+                regexCharacters.split('').forEach(function(character) {
+                    var characterAsRegex = new RegExp('(\\' + character + ')', 'g');
+                    pattern = pattern.replace(characterAsRegex, '\\$1');
+                });
+                pattern = new RegExp(pattern, 'g');
+            }
+            return pattern;
+        },
+        on: function(reg, callback) {
+            var reg = convertPatternToReg(reg);
+            if (reg.test(window.location.host)) {
+                callback.call(this);
+            }
+        }
+    };
+    macvideo.on('v.youku.com', function() {
+        var videoId = window.videoId;
+        if (!!videoId) {
+
+        }
+    });
+})(window);
