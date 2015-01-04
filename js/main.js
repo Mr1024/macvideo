1345ca7e05ea66eb2a9eae848f5a6ef7f7763e9e
zhangyizhong <zhangyizhong@news.cn>
2015-01-04 14:52:52 +0800

    update

--- js/main.js
+++ js/main.js
@@ -17,12 +17,69 @@
             if (reg.test(window.location.host)) {
                 callback.call(this);
             }
+        },
+        jsonp: function(url, callback, handler) {
+            var script = document.createElement("script");
+            var cache = handler || "mavvideo" + (new Date).getTime() + Math.random().toString().replace(".", "");
+            window[back] = function() {
+                callback && callback.apply(this, arguments);
+                try {
+                    script.parentNode.removeChild(script);
+                }
+                delete window[cache];
+            };
+            document.body.appendChild(script);
+            script.src = url + cache
         }
     };
     macvideo.on('v.youku.com', function() {
         var videoId = window.videoId;
+        var urlObj = {};
         if (!!videoId) {
+            if (macvideo.canPlayM3U8) {
+                urlObj = {
+                    "标清": "/player/getM3U8/vid/" + videoId + "/type/flv/ts/" + (((new Date).getTime() / 1e3).toString() | 0) + "/v.m3u8",
+                    "高清": "/player/getM3U8/vid/" + videoId + "/type/mp4/ts/" + (((new Date).getTime() / 1e3).toString() | 0) + "/v.m3u8",
+                    "超清": "/player/getM3U8/vid/" + videoId + "/type/hd2/ts/" + (((new Date).getTime() / 1e3).toString() | 0) + "/v.m3u8"
+                };
+            } else {
+                function getFileIDMixString(seed) {
+                    var source = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ/\\:._-1234567890".split(""),
+                        mixed = [],
+                        index;
+                    for (var i = 0, len = source.length; i < len; i++) {
+                        seed = (seed * 211 + 30031) % 65536;
+                        index = Math.floor(seed / 65536 * source.length);
+                        mixed.push(source[index]);
+                        source.splice(index, 1)
+                    }
+                    return mixed.join("")
+                }
 
+                function getFileID(fileid, seed) {
+                    var mixed = getFileIDMixString(seed),
+                        ids = fileid.split("*"),
+                        realId = [],
+                        idx;
+                    for (var i = 0; i < ids.length; i++) {
+                        idx = parseInt(ids[i], 10);
+                        realId.push(mixed.charAt(idx))
+                    }
+                    return realId.join("")
+                }
+                macvideo.jsonp("http://v.youku.com/player/getPlaylist/VideoIDS/" + videoId + "/Pf/4?__callback=", function(param) {
+                    var d = new Date,
+                        fileid = getFileID(param.data[0]["streamfileids"]["3gphd"], param.data[0]["seed"]),
+                        sid = d.getTime() + "" + (1e3 + d.getMilliseconds()) + "" + parseInt(Math.random() * 9e3),
+                        k = param.data[0]["segs"]["3gphd"][0]["k"],
+                        st = param.data[0]["segs"]["3gphd"][0]["seconds"];
+                    macvideo.jsonp("http://f.youku.com/player/getFlvPath/sid/" + sid + "_00/st/mp4/fileid/" + fileid + "?K=" + k + "&hd=1&myp=0&ts=1156&ypp=0&ymovie=1&callback=", function(param) {
+                        urlobj = {
+                            "高清": param[0]["server"]
+                        }
+                    })
+                })
+            }
         }
     });
 })(window);
